fb5b28d73b14216fd171bd99c30cabc47e338e84
Jorge <glph8911@gmail.com>
2023-07-28 13:04:33 -0500

    storybook config

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,11 +1,21 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  env: { browser: true, es2020: true },
+  extends: [
+    "next",
+    "turbo",
+    "prettier",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
+    semi: ["error", "never"]
   },
+  plugins: ["react-refresh"],
   parserOptions: {
     babelOptions: {
       presets: [require.resolve("next/babel")],
     },
   },
-};
+}
---
+++ packages/sb-workshop/.eslintrc.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  root: true,
+  extends: ["custom"],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+}
