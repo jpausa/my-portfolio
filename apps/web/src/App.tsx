115c2d26394d1b2651867b9647caf2ca0add816c
Jorge <glph8911@gmail.com>
2023-07-29 15:15:27 -0500

    merging sb with tailwind config to main

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,9 +1,12 @@
+import {Button} from 'sb-design-system/src/components/atoms/Button'
+
 function App() {
   return (
     <>
       <p className="bg-green-400">
         Portfolio
       </p>
+      <Button/>
     </>
   )
 }
